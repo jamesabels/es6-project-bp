a053479f7a728d5cbe59acc2a446cfa9fdecaacd
James Abels <jamesrabels@gmail.com>
2018-03-31 16:41:08 -0400

    inital commit

---
+++ karma.conf.js
@@ -0,0 +1,60 @@
+const path = require('path');
+
+module.exports = function(config) {
+    config.set({
+        basePath: '.',
+        frameworks: ['mocha', 'chai'],
+        browsers : ['jsdom'],
+        jsdomLauncher: {
+            jsdom: {
+              userAgent: "foobar"
+            }
+        },
+        files: [
+            { pattern: 'test/*_test.js', watched: true },
+            { pattern: 'test/**/*_test.js', watched: true }
+        ],
+        plugins: [
+            'karma-jsdom-launcher',
+            'karma-webpack',
+            'karma-mocha',
+            'karma-chai',
+            'karma-chrome-launcher'
+        ],
+        webpack: {
+            output: {
+              filename: 'bundle.js',
+              path: path.resolve(__dirname, 'dist')
+            },
+            devServer: {
+              contentBase: path.join(__dirname, "dist"),
+              compress: true,
+              port: 9000
+            },
+            module: {
+              rules: [
+                {
+                  test: /\.js$/,
+                  exclude: /(node_modules|bower_components)/,
+                  use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env']
+                    }
+                  }
+                }
+              ]
+            }
+        },
+        preprocessors: {
+            // add webpack as preprocessor
+            'test/*_test.js': ['webpack'],
+            'test/**/*_test.js': ['webpack']
+        },
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            // i. e.
+            stats: 'errors-only'
+        },
+    });
+};
---
+++ test/hello_world_test.js
@@ -0,0 +1,12 @@
+import chai from 'chai';
+import 'chai/register-should';
+import 'chai/register-expect';
+
+// Import functions
+import helloWorld from '../index';
+
+describe('Hello World', function () {
+    it ('Should log the passed Message', function () {
+        expect(helloWorld('Hello World')).to.equal('Hello World');
+    });
+});
